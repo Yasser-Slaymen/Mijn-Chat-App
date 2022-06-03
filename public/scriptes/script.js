4ec2f5827c4a7e58023658b52d2e7fec223d116e
Yasser-Slaymen <yaserslyman0@gmail.com>
2022-06-03 14:22:06 +0200

    update

---
+++ public/scriptes/script.js
@@ -0,0 +1,70 @@
+ let socket;
+ const messagesContainer = document.querySelector('.messagesContainer')
+ const messageForm = document.querySelector('#messageform')
+ const messageInput = document.querySelector('#messageInput')
+ const loginForm = document.querySelector('#loginForm')
+ const nameInput = document.querySelector('#nameIput')
+ let time = new Date().toLocaleTimeString([], {hour: '2-digit', minute:'2-digit'})
+
+//  Login bij name
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const name = nameInput.value
+    login(name)
+    // Remove the login form and
+    // show the chat message form
+    loginForm.remove()
+    messageForm.classList.remove('hidden')
+})
+
+function login(name){
+    socket = io()
+    socket.emit('login', name)
+    // Recieve Message
+    socket.on('msg', (data) => {
+        if (data.from != name) {
+            say(data.from, data.message)
+        } else {
+            say('You', data.message)
+        } 
+    })
+}
+
+ // Send Message $ Submit Form
+ messageForm.addEventListener('submit', function(event) {
+    event.preventDefault()
+    let message = messageInput.value;
+    messageInput.value = "";
+    // Send
+    socket.emit('msg',message)
+    
+})
+
+
+// Outputmeessage to Dom
+function say(name, message) {
+    const li = document.createElement('li')
+    li.classList.add('message')
+    li.innerHTML = 
+            `<li class="message">
+                <p class="meta"><span>${name}</span></p>
+                <p class="message-span"><span>${message}</span></p>
+                <p class="text"><span>${time}</span></p>
+            </li>`
+        
+    messagesContainer.appendChild(li)
+    // Scroll down to last message
+    messagesContainer.scrollTop = messagesContainer.scrollHeight
+}
+
+
+
+
+
+
+
+
+
+
+
+
